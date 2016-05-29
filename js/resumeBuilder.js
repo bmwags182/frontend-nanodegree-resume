a294ca8790b1951f6f1d18086b465a30d56add6a
bmwags182 <bretwagner@bwagner-webdev.com>
2016-05-28 19:51:28 -0500

    added some basic variables to the js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,26 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+This code was mostly written in order to fill the resume page 
+much of the code was written ver batim or very close from the 
+instructional videos. Some minor edits were made to replace 
+my specific information and styling.
  */
 
-$("#main").append("Bret Wagner");
+var name = "Bret Wagner ";
+var role = "Web Developer ";
+var email = "bretwagner@bwagner-webdev.com ";
+var mobile = "314-783-6696 ";
+var github = "https://github.com/bmwags182 ";
+var myLocation = "Greater St. Louis Area ";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedEmail = HTMLemail.replace("%data%" , email);
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+var formattedGithub = HTMLgithub.replace("%data%", github);
+var skills = ["HTML ", "CSS ", "javascript ", "Wordpress ", "Python ", "PHP ", ];
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+$("#lets-connect").append(formattedEmail);
+$("#header").append(skills);
+$("#header").append(myLocation);
